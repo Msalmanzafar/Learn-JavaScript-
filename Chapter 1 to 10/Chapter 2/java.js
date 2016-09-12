a14775fa177748a98043930f0b9a6785bfdc7eaf
msalmanzafar <m.salman.zafar@gmail.com>
2016-09-12 22:13:05 +0500

    chapter 2

---
+++ Chapter 1 to 10/Chapter 2/java.js
@@ -0,0 +1,11 @@
+alert("Thanks for Chosen");
+
+var Fname= "Salman ";
+var Lname= "Zafar";
+alert("Created by " + Fname + Lname);
+
+var a= "I ";
+var b= "Hope ";
+var c= "You ";
+var d= "Understand ";
+alert(a + b + c + d + " about JavaScript");
