fa3019c744f191fa4d4b4703a6280cbc427b65a7
msalmanzafar <m.salman.zafar@gmail.com>
2016-09-13 17:29:05 +0500

    updates about javascripts

---
+++ Chapter-1-to-10/Chapter-3/java.js
@@ -0,0 +1,18 @@
+alert("Thanks for Chosen");
+
+var a = 10;
+var b = 13;
+alert("10 + 13 = " + (a + b));
+alert("10 - 13 = " + (a - b));
+
+var x = 12;
+var y = 12;
+alert( "12 * 12 = " + (x * y));
+
+var x = 9;
+var y = 2;
+alert( "9 / 2 = " + (x / y));
+
+var x = 22;
+var y = 3;
+alert( "22 % 3 = " + (x % y));
