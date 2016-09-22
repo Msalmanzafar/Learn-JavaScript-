3ad98c7df3e00b8723fc2a01d14416b774c0a1ea
msalmanzafar <m.salman.zafar@gmail.com>
2016-09-22 11:47:10 +0500

    lab task complete

--- Chapter-1-to-10/Chapter-10/java.js
+++ Chapter-1-to-10/Chapter-10/java.js
@@ -1,2 +1,45 @@
 alert("Thanks for Chosen");
 
+var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+    alert("Correct!");
+ }
+ else{
+	 alert("Wrong Answer!");
+ }
+ 
+ /*var scoreAvg = Math.round(2.7);
+ alert(scoreAvg);
+ 
+ var randomNumber = Math.random();
+ alert(randomNumber);
+ 
+ var bigDecimal = Math.random();
+ var improvedNum = (bigDecimal * 6) + 1;
+ var numberOfStars = Math.floor(improvedNum);
+ alert("Big Decimal " + numberOfStars);
+ 
+ var q = prompt("Enter your Age !");
+ var num = Number(q);
+ alert("Your Age is: "+num);
+ 
+ var rightNow = new Date();
+ alert(rightNow);
+ 
+ var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+ var now = new Date();
+ var theDay = now.getDay();
+ var nameOfToday = dayNames[theDay];
+ alert(nameOfToday);
+ 
+ var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();
+ var daysTillDoom = Mathfloor(msDiff / (1000 * 60 * 60 * 24));
+ alert(msDiff);*/
+ 
+ function tellTime() {
+ var now = new Date();
+ var theHr = now.getHours();
+ var theMin = now.getMinutes();
+ alert("Current time: "+ theHr + ":" + theMin);
+ }
+ alert(tellTime());
--- Chapter-1-to-10/Chapter-9/java.js
+++ Chapter-1-to-10/Chapter-9/java.js
@@ -3,5 +3,18 @@ alert("Thanks for Chosen");
 var question = "Enter Your Name: ";
 var defaultAnswer = "Sky User";
 var spec = prompt(question, defaultAnswer);
-
 alert("Thanks " + spec);
+
+var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+var cityToCheck = "Cheyenne";
+var matchFound = "no";
+for (var i = 0; i <= 4; i++) {
+ if (cityToCheck === cleanestCities[i]) {
+	 matchFound = "yes";
+ alert(cityToCheck + " It's one of the cleanest cities");
+ }
+ else if(matchFound === "no"){
+	 alert("It's not on the list");
+ }
+ }
+ 
