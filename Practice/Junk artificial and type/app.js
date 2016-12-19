35b8b4a8512c64dfd4f7391646825592711626bf
msalmanzafar <m.salman.zafar@gmail.com>
2016-12-20 01:54:55 +0500

    responsive

---
+++ Practice/Junk artificial and type/app.js
@@ -0,0 +1,44 @@
+/*how to edited and modified text(Nodes) using for loop*/
+
+// var d = document.getElementById("humpty");
+
+//  var pCounter = 0;
+//  for (var i = 0; i < d.childNodes.length; i++) {
+//  if (d.childNodes[i].nodeType === 1 ) {
+//  pCounter++;
+//  }
+//  if (pCounter === 2) {
+//  d.childNodes[i].innerHTML = "All his men.";
+// }
+//  if (pCounter === 4) {
+//  d.childNodes[i].innerHTML += " and Salman best friends.";
+//  break;
+//  }
+//  }
+
+
+
+/*----Take inputs from users 1 & 2 and print it through tables and swape Profile of user 1 & 2-----*/
+
+// function data(){
+//      fName = document.getElementById('FName').value;
+//      fProfile = document.getElementById('FProfile').value;
+//      sName = document.getElementById('SName').value;
+//      sProfile = document.getElementById('SProfile').value;
+//      div = document.getElementById('box');
+//      tabl();
+// }
+// function tabl(){
+//      div.innerHTML = "<table><tr><th>First User Name</th><th>Profile</th></tr><tr><td>"+fName+"</td><td>"
+//      +fProfile+"</td></tr></table> <br> <hr> <button type='button' onclick='inter()'>InterChange</button> <table><tr><th>First User Name</th><th>Profile</th></tr><tr><td>"+sName+"</td><td>"
+//      +sProfile+"</td></tr></table>";
+
+// }
+// function inter(){
+//     Ifp = fProfile;
+//     Isp = sProfile;
+//     fProfile = Isp;
+//     sProfile = Ifp;
+//     tabl();
+// }
+
