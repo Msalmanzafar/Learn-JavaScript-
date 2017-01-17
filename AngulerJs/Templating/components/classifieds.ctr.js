2e9dfdb95465b34b02b8a749263d6ceaf1b869c5
msalmanzafar <m.salman.zafar@gmail.com>
2017-01-17 18:02:02 +0500

    angular understanding

--- AngulerJs/Templating/components/classifieds.ctr.js
+++ AngulerJs/Templating/components/classifieds.ctr.js
@@ -3,122 +3,59 @@
     
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl",function($scope){
-            $scope.classifieds =[
-                {
-                    "id": "1",
-                    "title": "20 foot Equipment Trailer",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 600,
-                    "posted": "2015-10-24",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/1.jpg",
-                    "views": 201
-                },
-                {
-                    "id": "2",
-                    "title": "Moto Z Force",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 755,
-                    "posted": "2016-11-23",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/download.jpg",
-                    "views": 201
-                },
-                {
-                    "id": "3",
-                    "title": "Moto X Play",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 590,
-                    "posted": "2016-02-11",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/2.jpg",
-                    "views": 201
-                },
-                {
-                    "id": "4",
-                    "title": "20 foot Equipment Trailer",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 790,
-                    "posted": "2015-10-24",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/3.jpg",
-                    "views": 201
-                },
-                {
-                    "id": "5",
-                    "title": "20 foot Equipment Trailer",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 765,
-                    "posted": "2015-10-24",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/4.jpg",
-                    "views": 201
-                },
-                {
-                    "id": "6",
-                    "title": "20 foot Equipment Trailer",
-                    "id": "2013 rainbow trailer 20 feet x82 inch",
-                    "price": 691,
-                    "posted": "2015-10-24",
-                    "description": "Traditionally, the view modifies the DOM to present data and manipulates the DOM (or invokes jQuery) to add behavior. With Angular, ",
-                    "contact":{
-                        "name": "Sami",
-                        "phone":"(122) 255-5555",
-                        "email": "sam@gmail.com" 
-                    },
-                    "categories":[
-                        "Vehicles",
-                        "Parts and Accessories"
-                    ],
-                    "image": "images/5.jpg",
-                    "views": 201
+        .controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdSidenav,$mdToast, $mdDialog){
+            
+            classifiedsFactory.getClassifieds().then(function(classifieds){
+                $scope.classifieds = classifieds.data;
+                //console.log(data);
+            });
+            $scope.openSidebar = function(){
+                $mdSidenav('left').open();
+            };
+            $scope.closeSidebar = function(){
+                $mdSidenav('left').close();
+            };
+            $scope.saveClassified = function(classified){
+                if(classified){
+                    $scope.classifieds.push(classified);
+                    $scope.classified ={};
+                    $scope.closeSidebar();
+                    showToast("Classified Saved!");
                 }
-            ]
+            }
+
+            $scope.editClassified = function(classified){
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.classified = classified;
+            }
+            $scope.saveEdit =function(){
+                $scope.editing = false;
+                $scope.classified ={};
+                $scope.closeSidebar();
+                showToast("Edit Saved!");
+            }
+            $scope.deleteClassified = function(event,classified){
+                var confirm = $mdDialog.confirm()
+                    .title("Are your you want to delete "+ classified.title + '?')
+                    .ok("yes")
+                    .cancel("No")
+                    .targetEvent(event);
+                    $mdDialog.show(confirm).then(function(){
+                        var index = $scope.classifieds.indexOf(classified);
+                        $scope.classifieds.splice(index , 1);
+                    },function(){
+
+                    });
+            }
+
+            function showToast(message){
+                $mdToast.show(
+                        $mdToast.simple()
+                        .content(message)
+                        .position('top, right')
+                        .hideDelay(3000)
+                    );
+            }
         });
 })();
