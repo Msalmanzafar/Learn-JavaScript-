24cae2aa85a5c4bd80bb487493a2de42b306743e
msalmanzafar <m.salman.zafar@gmail.com>
2017-01-25 12:02:10 +0500

    updates

--- AngulerJs/Templating/script/app.js
+++ AngulerJs/Templating/script/app.js
@@ -1,26 +1,109 @@
-(function(){
-    "use strict";
+// (function(){
+//     "use strict";
+//
+//     angular.module("samApp",['ngMaterial','ngAria','ngAnimate', 'ngMessages'])
+//         .config(function($mdThemingProvider){
+//             $mdThemingProvider.theme('default')
+//                 .primaryPalette('blue')
+//                 .accentPalette('orange');
+//         })
+//
+//
+//         .controller("listController",function($scope, $http){
+//             $http.get('data/signup.json')
+//                 .then(function(response){
+//                     $scope.myData = response.data;
+//
+//                     // var ages = [];
+//                     // var userAges = [];
+//                     // angular.forEach($scope.myData , function (age) {
+//                     //     ages.push(age.age);
+//                     //     userAges = ages.filter(function (val) {
+//                     //         return
+//                     //     })
+//                     //
+//                     // });
+//                     // //$scope.ages = ages;
+//                     // console.log(userAges);
+//
+//         });
+//
+//
+//
+//
+//             var formData=[];
+//             $scope.submitForm = function(form) {
+//                 formData.push($scope.form);
+//                 $scope.form = {};
+//                 form.$setUntouched();
+//                 form.$setPristine();
+//
+//             };
+//         });
+// })();
 
-    angular.module("samApp",['ngMaterial','ngAria','ngAnimate', 'ngMessages'])
-        .config(function($mdThemingProvider){
-            $mdThemingProvider.theme('default')
-                .primaryPalette('blue')
-                .accentPalette('orange');
+var app = angular.module('user',['ngMaterial']);
+    app.controller("myUser", function($scope) {
+        $scope.name = "salman";
+       $scope.userInfo ={
+           name: "Salman",
+           address: "Satadium Road Karachi",
+           nationality: "Pakistani"
+       }
+    })
+    //-------------------Custom-Directive----------------------------------
+    // .directive("firstHeading",function () {
+    //     return {
+    //         template: "<h1>This is my First Heading</h1>"
+    //     }
+    // })
+    // .directive("secondHeading",function () {
+    //     return {
+    //         restrict: 'A',
+    //         template: "<h2>This is my Second Heading</h2>"
+    //     }
+    // })
+    // .directive("thirdHeading",function () {
+    //     return {
+    //         restrict: 'C',
+    //         template: "<h3>This is my Third Heading</h3>"
+    //     }
+    // })
+    // .directive("fourthHeading",function () {
+    //     return {
+    //         restrict: 'M',
+    //         template: "<h4>This is my Fourth Heading</h4>"
+    //     }
+    // })
+    //--------------------get data from controller------------------
+    .directive("newUser", function () {
+        return{
+            scope: {
+                name:'@'
+            },
+            template: '<input ng-model="name"><h2>Name: {{name}}'+"<br/>"+' Address: {{address}}</h2>'
+        }
+    })
+        .directive("mdCheck", function () {
+            return{
+                scope: {
+                    name:'='
+                },
+                template: '<input ng-model="name"><h2>Name: {{name}}'+"<br/>"+' Address: {{address}}</h2>'
+            }
         })
-        .controller("listController",function($scope, $http){
-            $http.get('data/signup.json')
-                .then(function(response){
-                    $scope.myData = response.data;
-                    console.log($scope.myData);
-        });
-            
-            // $scope.submitForm = function(form) {
-            //     formData.push($scope.form);
-            //     $scope.form = {};
-            //     form.$setUntouched();
-            //     form.$setPristine();
-            //     console.log(formData);
-            // }
-        });
-})();
+        .directive("mdInfo", function () {
+            return{
+                scope: {
+                    name:'&'
+                },
+                template: '<input ng-model="name"><h2>Name: {{name}}'+"<br/>"+' Address: {{address}}</h2>'
+            }
+        })
+        .directive("getData", function () {
+            return{
+                templateUrl: 'my_customer.html'
+            }
+        })
+
 
